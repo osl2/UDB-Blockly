75f9336c7141ed5c8ca8430377dbe10cb3c0d246
Marcus <marcus@marcusleib.com>
2019-07-10 16:12:47 +0200

    Modifier für column_definition

--- custom/columndefinition.js
+++ custom/columndefinition.js
@@ -29,10 +29,35 @@ Blockly.Blocks['columndefinition'] = {
 Blockly.Blocks['columndefinition_type'] = {
     init: function () {
         this.appendDummyInput('input')
-            .appendField(new Blockly.FieldDropdown([["INTEGER", "integer"], ["DOUBLE", "double"], ["FLOAT", "float"], ["BOOLEAN", "boolean"], ["DATE", "date"], ["DATETIME", "datetime"], ["VARCHAR", "varchar"]]), "TYPE");
+            .appendField(new Blockly.FieldDropdown([["INTEGER", "integer"], ["DOUBLE", "double"], ["FLOAT", "float"], ["BOOLEAN", "boolean"], ["DATE", "date"], ["DATETIME", "datetime"], ["VARCHAR", "varchar"]], function (selectedType) {
+                this.sourceBlock_.updateShape_(selectedType)
+            }), "TYPE");
         this.setOutput(true, "columntype");
         this.setColour(230);
         this.setTooltip("");
         this.setHelpUrl("");
+    },
+    mutationToDom: function () {
+        let container = document.createElement('mutation');
+        container.setAttribute('type', this.getFieldValue('TYPE'));
+        return container;
+    },
+    domToMutation: function (xmlElement) {
+        let value = xmlElement.getAttribute('type');
+        this.setFieldValue(value, 'TYPE');
+        this.updateShape_(value);
+    },
+    updateShape_: function (selectedType) {
+        //Add or remove field for entry of n in varchar(n)
+        let input = this.getInput('input');
+        let fieldExists = this.getField('varcharlength');
+        if (selectedType === "varchar") {
+            if (!fieldExists) {
+                input.appendField(new Blockly.FieldNumber(0, 0, Infinity, 1), "varcharlength");
+            }
+        } else if (fieldExists) {
+            //remove it, it is not needed
+            input.removeField('varcharlength');
+        }
     }
 };
