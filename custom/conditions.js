4aeddf08e62859d302d02298362580f1723369e5
Marcus <marcus@marcusleib.com>
2019-07-10 16:55:14 +0200

    Modifier für LIKE und IN

--- custom/conditions.js
+++ custom/conditions.js
@@ -3,7 +3,9 @@ Blockly.Blocks['cond_block'] = {
         this.appendValueInput("column")
             .setCheck(["column", "literal"]);
         this.appendDummyInput()
-            .appendField(new Blockly.FieldDropdown([["=", "1"], ["<>", "2"], [">", "3"], ["<", "4"], [">=", "5"], ["<=", "6"], ["LIKE", "7"], ["IN", "8"]]), "NAME");
+            .appendField(new Blockly.FieldDropdown([["=", "1"], ["<>", "2"], [">", "3"], ["<", "4"], [">=", "5"], ["<=", "6"], ["LIKE", "like"], ["IN", "in"]], function (selection) {
+                this.sourceBlock_.updateSelection_(selection)
+            }), "OPERATOR");
         this.appendValueInput("value")
             .setCheck(["column", "literal"]);
         this.setInputsInline(false);
@@ -12,6 +14,31 @@ Blockly.Blocks['cond_block'] = {
         this.setColour(230);
         this.setTooltip("");
         this.setHelpUrl("");
+    },
+    updateSelection_: function (selection) {
+        //If LIKE is selected the input on the second needs to be a String.
+        //If IN is selected the input on the second needs to be a SELECT Statement or list of values
+        if (selection === "like") {
+            if (this.getInput("value").type !== Blockly.INPUT_VALUE) {
+                this.removeInput("value");
+                this.appendValueInput("value")
+                    .setCheck("string");
+            } else {
+                this.getInput("value").setCheck("string");
+            }
+        } else if (selection === "in") {
+            if (this.getInput("value").type === Blockly.INPUT_VALUE) {
+                this.removeInput("value");
+                this.appendStatementInput("value")
+                    .setCheck(["value", "subselect"]);
+            }
+        } else {
+            if (this.getInput("value").type !== Blockly.INPUT_VALUE) {
+                this.removeInput("value");
+                this.appendValueInput("value")
+                    .setCheck(["column", "literal"]);
+            }
+        }
     }
 };
 
