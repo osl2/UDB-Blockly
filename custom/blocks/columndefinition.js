0582c4c38883b710179e4403e3317d66f5311f61
Marcus <marcus@marcusleib.com>
2019-07-17 15:42:42 +0200

    Umstrukturierung columndefinition

--- custom/blocks/columndefinition.js
+++ custom/blocks/columndefinition.js
@@ -1,24 +1,31 @@
 Blockly.Blocks['columndefinition'] = {
     init: function () {
-        this.appendValueInput("column")
-            .setCheck("columntype")
-            .appendField(new Blockly.FieldTextInput("columnname"), "colname");
+        this.appendDummyInput("COLUMN")
+            .appendField("Columnname:")
+            .appendField(new Blockly.FieldTextInput("columnname"),"NAME");
+        this.appendDummyInput("TYPE")
+            .appendField("Columntype:")
+            .appendField(new Blockly.FieldDropdown([["TEXT", "TEXT"], ["NUMERIC", "NUMERIC"], ["INTEGER", "INTEGER"], ["REAL", "REAL"],  ["BLOB", "BLOB"]]), "TYPE");
+        this.appendDummyInput();
         this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_LEFT)
+            .appendField("Constraints:");
+        this.appendDummyInput("PRIMARYKEY")
             .appendField("PRIMARY KEY")
             .appendField(new Blockly.FieldCheckbox("FALSE", function (isChecked) {
                 this.sourceBlock_.selectPrimaryKey_(isChecked)
-            }), "primkey")
+            }), "primarykey")
             .appendField("AUTOINCREMENT")
             .appendField(new Blockly.FieldCheckbox("FALSE", function (isChecked) {
                 this.sourceBlock_.selectAutoincrement_(isChecked)
             }), "autoincrement");
-        this.appendDummyInput()
+        this.appendDummyInput("NOT NULL")
             .appendField("NOT NULL")
-            .appendField(new Blockly.FieldCheckbox("FALSE"), "null");
-        this.appendDummyInput()
+            .appendField(new Blockly.FieldCheckbox("FALSE"), "notnull");
+        this.appendDummyInput("UNIQUE")
             .appendField("UNIQUE")
             .appendField(new Blockly.FieldCheckbox("FALSE"), "unique");
-        this.appendValueInput("def")
+        this.appendValueInput("DEFAULT")
             .setCheck("value")
             .appendField("DEFAULT")
             .appendField(new Blockly.FieldCheckbox("FALSE"), "default");
@@ -31,49 +38,13 @@ Blockly.Blocks['columndefinition'] = {
     selectAutoincrement_: function (isChecked) {
         //If AUTOINCREMENT is selected, PRIMARY KEY needs to be selected as well.
         if (isChecked) {
-            this.getField('primkey').setValue(true);
+            this.getField('primarykey').setValue(true);
         }
     },
     selectPrimaryKey_: function (isChecked) {
         //If PRIMARY KEY is deselected, AUTOINCREMENT needs to be deselected as well.
         if (!isChecked) {
             this.getField('autoincrement').setValue(false);
-        }
-    }
-};
-
-Blockly.Blocks['columndefinition_type'] = {
-    init: function () {
-        this.appendDummyInput('input')
-            .appendField(new Blockly.FieldDropdown([["INTEGER", "integer"], ["DOUBLE", "double"], ["FLOAT", "float"], ["BOOLEAN", "boolean"], ["DATE", "date"], ["DATETIME", "datetime"], ["VARCHAR", "varchar"]], function (selectedType) {
-                this.sourceBlock_.updateShape_(selectedType)
-            }), "TYPE");
-        this.setOutput(true, "columntype");
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    },
-    mutationToDom: function () {
-        let container = document.createElement('mutation');
-        container.setAttribute('type', this.getFieldValue('TYPE'));
-        return container;
-    },
-    domToMutation: function (xmlElement) {
-        let value = xmlElement.getAttribute('type');
-        this.setFieldValue(value, 'TYPE');
-        this.updateShape_(value);
-    },
-    updateShape_: function (selectedType) {
-        //Add or remove field for entry of n in varchar(n)
-        let input = this.getInput('input');
-        let fieldExists = this.getField('varcharlength');
-        if (selectedType === "varchar") {
-            if (!fieldExists) {
-                input.appendField(new Blockly.FieldNumber(0, 0, Infinity, 1), "varcharlength");
-            }
-        } else if (fieldExists) {
-            //remove it, it is not needed
-            input.removeField('varcharlength');
         }
     }
 };
--- custom/generators/sql/columndefinition.js
+++ custom/generators/sql/columndefinition.js
@@ -1,7 +1,26 @@
 Blockly.SQL['columndefinition'] = function(block) {
-    return code;
-};
+    var code = "";
+    var name = block.getFieldValue('NAME');
+    var type = block.getFieldValue('TYPE');
+    var constraintlist = "";
+    var defaultvalue = Blockly.SQL.valueToCode(block, 'DEFAULT', Blockly.SQL.ORDER_ATOMIC).trim();
 
-Blockly.SQL['columndefinition_type'] = function(block) {
+    //Build Constraintlist
+    if (block.getField('primarykey').getValue() === "TRUE") {
+        constraintlist += "PRIMARY KEY";
+        if (block.getField('autoincrement').getValue() === "TRUE") {
+            constraintlist += " AUTOINCREMENT";
+        }
+    }
+    if (block.getField('notnull').getValue() === "TRUE") {
+        constraintlist += " NOT NULL";
+    }
+    if (block.getField('unique').getValue() === "TRUE") {
+        constraintlist += " UNIQUE";
+    }
+    if (block.getField('default').getValue() === "TRUE") {
+        constraintlist += " DEFAULT " + defaultvalue;
+    }
+    code += name + " " + type + " " + constraintlist.trim() + "\n";
     return code;
 };
