16906bbc102ef4a81f5dc8aec5f442dc32bc30f0
Marcus <marcus@marcusleib.com>
2019-07-24 15:47:46 +0200

    Wrap aus Blockly.util entfernen

--- custom/generators/sql.js
+++ custom/generators/sql.js
@@ -112,7 +112,6 @@ Blockly.SQL.scrub_ = function (block, code, opt_thisOnly) {
     if (!block.outputConnection || !block.outputConnection.targetConnection) {
         // Collect comment for this block.
         var comment = block.getCommentText();
-        comment = Blockly.utils.wrap(comment, Blockly.SQL.COMMENT_WRAP - 3);
         if (comment) {
             // Use a comment block for function comments.
             commentCode += '/*\n' +
