77c6098280336e6dafe4628b8b08afb010c068ec
Marcus <marcus@marcusleib.com>
2019-08-11 19:56:46 +0200

    Füge Verbildlichungen der Datentypen hinzu.

--- build/blocks_custom.js
+++ build/blocks_custom.js
@@ -390,6 +390,7 @@ Blockly.Blocks['cond_and'] = {
 Blockly.Blocks['data_boolean'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u22A8 / \u22AD")
             .appendField(new Blockly.FieldDropdown([["true", "TRUE"], ["false", "FALSE"]]), "BOOLEAN");
         this.setOutput(true, ["boolean", "value"]);
         this.setColour(230);
@@ -401,6 +402,7 @@ Blockly.Blocks['data_boolean'] = {
 Blockly.Blocks['data_boolean_statement'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u22A8 / \u22AD")
             .appendField(new Blockly.FieldDropdown([["true", "TRUE"], ["false", "FALSE"]]), "BOOLEAN");
         this.setPreviousStatement(true, ["boolean", "value"]);
         this.setNextStatement(true, ["boolean", "value"]);
@@ -413,9 +415,17 @@ Blockly.Blocks['data_boolean_statement'] = {
 Blockly.Blocks['data_string'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField(new Blockly.FieldImage("media/quote0.png", 15, 10, "*"))
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "n0lEQVQI1z3OMa5BURSF4f/cQhAKjUQhuQmFNwGJEUi0RKN5rU7FHKhpjEH3TEMtkdBSCY" +
+                "1EIv8r7nFX9e29V7EBAOvu7RPjwmWGH/VuF8CyN9/OAdvqIXYLvtRaNjx9mMTDyo+NjAN1" +
+                "HNcl9ZQ5oQMM3dgDUqDo1l8DzvwmtZN7mnD+PkmLa+4mhrxVA9fRowBWmVBhFy5gYEjKMf" +
+                "z9AylsaRRgGzvZAAAAAElFTkSuQmCC", 15, 10, "*"))
             .appendField(new Blockly.FieldTextInput("value"), "STRING")
-            .appendField(new Blockly.FieldImage("media/quote1.png", 15, 10, "*"));
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "qUlEQVQI1z3KvUpCcRiA8ef9E4JNHhI0aFEacm1o0BsI0Slx8wa8gLauoDnoBhq7DcfWhg" +
+                "gONDmJJgqCPA7neJ7p934EOOKOnM8Q7PDElo/4x4lFb2DmuUjcUzS3URnGib9qaPNbuXvB" +
+                "O3sGPHJDRG6fGVdMSeWDP2q99FQdFrz26Gu5Tq7dFMzUvbXy8KXeAj57cOklgA+u1B5Aos" +
+                "lLtGIHQMaCVnwDnADZIFIrXsoXrgAAAABJRU5ErkJggg==", 15, 10, "*"));
         this.setOutput(true, ["string", "value"]);
         this.setColour(230);
         this.setTooltip(Blockly.Msg.Custom.DATAELEMENT_STRING);
@@ -426,9 +436,17 @@ Blockly.Blocks['data_string'] = {
 Blockly.Blocks['data_string_statement'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField(new Blockly.FieldImage("media/quote0.png", 15, 10, "*"))
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "n0lEQVQI1z3OMa5BURSF4f/cQhAKjUQhuQmFNwGJEUi0RKN5rU7FHKhpjEH3TEMtkdBSCY" +
+                "1EIv8r7nFX9e29V7EBAOvu7RPjwmWGH/VuF8CyN9/OAdvqIXYLvtRaNjx9mMTDyo+NjAN1" +
+                "HNcl9ZQ5oQMM3dgDUqDo1l8DzvwmtZN7mnD+PkmLa+4mhrxVA9fRowBWmVBhFy5gYEjKMf" +
+                "z9AylsaRRgGzvZAAAAAElFTkSuQmCC", 15, 10, "*"))
             .appendField(new Blockly.FieldTextInput("value"), "STRING")
-            .appendField(new Blockly.FieldImage("media/quote1.png", 15, 10, "*"));
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "qUlEQVQI1z3KvUpCcRiA8ef9E4JNHhI0aFEacm1o0BsI0Slx8wa8gLauoDnoBhq7DcfWhg" +
+                "gONDmJJgqCPA7neJ7p934EOOKOnM8Q7PDElo/4x4lFb2DmuUjcUzS3URnGib9qaPNbuXvB" +
+                "O3sGPHJDRG6fGVdMSeWDP2q99FQdFrz26Gu5Tq7dFMzUvbXy8KXeAj57cOklgA+u1B5Aos" +
+                "lLtGIHQMaCVnwDnADZIFIrXsoXrgAAAABJRU5ErkJggg==", 15, 10, "*"));
         this.setPreviousStatement(true, ["string", "value"]);
         this.setNextStatement(true, ["string", "value"]);
         this.setColour(230);
@@ -440,6 +458,7 @@ Blockly.Blocks['data_string_statement'] = {
 Blockly.Blocks['data_number'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u0023")
             .appendField(new Blockly.FieldTextInput("0", checkNumeric), "NUMBER");
         this.setOutput(true, ["number", "value"]);
         this.setColour(230);
@@ -451,6 +470,7 @@ Blockly.Blocks['data_number'] = {
 Blockly.Blocks['data_number_statement'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u0023")
             .appendField(new Blockly.FieldTextInput("0", checkNumeric), "NUMBER");
         this.setPreviousStatement(true, ["number", "value"]);
         this.setNextStatement(true, ["number", "value"]);
@@ -539,6 +559,7 @@ function checkNumeric(string) {
         return null;
     }
 }
+
 Blockly.Blocks['entity_table'] = {
     init: function () {
         this.appendDummyInput()
--- custom/blocks/data.js
+++ custom/blocks/data.js
@@ -1,6 +1,7 @@
 Blockly.Blocks['data_boolean'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u22A8 / \u22AD")
             .appendField(new Blockly.FieldDropdown([["true", "TRUE"], ["false", "FALSE"]]), "BOOLEAN");
         this.setOutput(true, ["boolean", "value"]);
         this.setColour(230);
@@ -12,6 +13,7 @@ Blockly.Blocks['data_boolean'] = {
 Blockly.Blocks['data_boolean_statement'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u22A8 / \u22AD")
             .appendField(new Blockly.FieldDropdown([["true", "TRUE"], ["false", "FALSE"]]), "BOOLEAN");
         this.setPreviousStatement(true, ["boolean", "value"]);
         this.setNextStatement(true, ["boolean", "value"]);
@@ -24,9 +26,17 @@ Blockly.Blocks['data_boolean_statement'] = {
 Blockly.Blocks['data_string'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField(new Blockly.FieldImage("media/quote0.png", 15, 10, "*"))
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "n0lEQVQI1z3OMa5BURSF4f/cQhAKjUQhuQmFNwGJEUi0RKN5rU7FHKhpjEH3TEMtkdBSCY" +
+                "1EIv8r7nFX9e29V7EBAOvu7RPjwmWGH/VuF8CyN9/OAdvqIXYLvtRaNjx9mMTDyo+NjAN1" +
+                "HNcl9ZQ5oQMM3dgDUqDo1l8DzvwmtZN7mnD+PkmLa+4mhrxVA9fRowBWmVBhFy5gYEjKMf" +
+                "z9AylsaRRgGzvZAAAAAElFTkSuQmCC", 15, 10, "*"))
             .appendField(new Blockly.FieldTextInput("value"), "STRING")
-            .appendField(new Blockly.FieldImage("media/quote1.png", 15, 10, "*"));
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "qUlEQVQI1z3KvUpCcRiA8ef9E4JNHhI0aFEacm1o0BsI0Slx8wa8gLauoDnoBhq7DcfWhg" +
+                "gONDmJJgqCPA7neJ7p934EOOKOnM8Q7PDElo/4x4lFb2DmuUjcUzS3URnGib9qaPNbuXvB" +
+                "O3sGPHJDRG6fGVdMSeWDP2q99FQdFrz26Gu5Tq7dFMzUvbXy8KXeAj57cOklgA+u1B5Aos" +
+                "lLtGIHQMaCVnwDnADZIFIrXsoXrgAAAABJRU5ErkJggg==", 15, 10, "*"));
         this.setOutput(true, ["string", "value"]);
         this.setColour(230);
         this.setTooltip(Blockly.Msg.Custom.DATAELEMENT_STRING);
@@ -37,9 +47,17 @@ Blockly.Blocks['data_string'] = {
 Blockly.Blocks['data_string_statement'] = {
     init: function () {
         this.appendDummyInput()
-            .appendField(new Blockly.FieldImage("media/quote0.png", 15, 10, "*"))
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "n0lEQVQI1z3OMa5BURSF4f/cQhAKjUQhuQmFNwGJEUi0RKN5rU7FHKhpjEH3TEMtkdBSCY" +
+                "1EIv8r7nFX9e29V7EBAOvu7RPjwmWGH/VuF8CyN9/OAdvqIXYLvtRaNjx9mMTDyo+NjAN1" +
+                "HNcl9ZQ5oQMM3dgDUqDo1l8DzvwmtZN7mnD+PkmLa+4mhrxVA9fRowBWmVBhFy5gYEjKMf" +
+                "z9AylsaRRgGzvZAAAAAElFTkSuQmCC", 15, 10, "*"))
             .appendField(new Blockly.FieldTextInput("value"), "STRING")
-            .appendField(new Blockly.FieldImage("media/quote1.png", 15, 10, "*"));
+            .appendField(new Blockly.FieldImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAKCAQAAAAqJXdxAAAA" +
+                "qUlEQVQI1z3KvUpCcRiA8ef9E4JNHhI0aFEacm1o0BsI0Slx8wa8gLauoDnoBhq7DcfWhg" +
+                "gONDmJJgqCPA7neJ7p934EOOKOnM8Q7PDElo/4x4lFb2DmuUjcUzS3URnGib9qaPNbuXvB" +
+                "O3sGPHJDRG6fGVdMSeWDP2q99FQdFrz26Gu5Tq7dFMzUvbXy8KXeAj57cOklgA+u1B5Aos" +
+                "lLtGIHQMaCVnwDnADZIFIrXsoXrgAAAABJRU5ErkJggg==", 15, 10, "*"));
         this.setPreviousStatement(true, ["string", "value"]);
         this.setNextStatement(true, ["string", "value"]);
         this.setColour(230);
@@ -51,6 +69,7 @@ Blockly.Blocks['data_string_statement'] = {
 Blockly.Blocks['data_number'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u0023")
             .appendField(new Blockly.FieldTextInput("0", checkNumeric), "NUMBER");
         this.setOutput(true, ["number", "value"]);
         this.setColour(230);
@@ -62,6 +81,7 @@ Blockly.Blocks['data_number'] = {
 Blockly.Blocks['data_number_statement'] = {
     init: function () {
         this.appendDummyInput()
+            .appendField("\u0023")
             .appendField(new Blockly.FieldTextInput("0", checkNumeric), "NUMBER");
         this.setPreviousStatement(true, ["number", "value"]);
         this.setNextStatement(true, ["number", "value"]);
